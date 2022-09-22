562da7290a007d127d7b2eb2cfdcfea1b7c19877
Krishna varma <kvarma00011@gmail.com>
2022-09-23 02:39:08 +0530

    Day 6 - Done with Footer Section

--- crypto-world/src/App.js
+++ crypto-world/src/App.js
@@ -8,6 +8,7 @@ import CoinPage from './routes/CoinPage';
 import Signup from './routes/Signup';
 import Account from './routes/Account';
 import axios from 'axios';
+import Footer from './components/Footer';
 
 function App() {
   const [coins, setCoins] = useState([]);
@@ -33,6 +34,7 @@ function App() {
           <Route path='coinId' />
         </Route> 
       </Routes>
+      <Footer />
     </ThemeProvider>
   );
 }
